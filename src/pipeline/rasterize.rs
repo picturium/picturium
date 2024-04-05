6a338e7ae0da20b08564491102085981a79fb552
lamka02sk <mail@samuelillo.com>
2024-04-05 19:47:05 +0200

    Improve rasterization

--- src/pipeline/rasterize.rs
+++ src/pipeline/rasterize.rs
@@ -2,15 +2,19 @@ use libvips::{ops, VipsImage};
 
 use crate::parameters::UrlParameters;
 use crate::pipeline::{PipelineError, PipelineResult};
-use crate::pipeline::resize::get_dimensions;
+use crate::pipeline::resize::{get_dimensions, get_original_dimensions};
 use crate::services::vips::get_error_message;
 
 // Rasterize SVG image to bitmap
 pub(crate) async fn run(image: &VipsImage, url_parameters: &UrlParameters<'_>) -> PipelineResult<VipsImage> {
 
     let (width, _) = get_dimensions(image, url_parameters);
+    let (original_width, original_height) = get_original_dimensions(image);
 
-    match ops::thumbnail(&url_parameters.path.to_string_lossy(), width) {
+    let ratio = original_width as f64 / original_height as f64;
+    let width = width as f64 * ratio;
+
+    match ops::thumbnail(&url_parameters.path.to_string_lossy(), width.ceil() as i32) {
         Ok(image) => Ok(image),
         Err(_) => Err(PipelineError(format!("Failed to rasterize SVG image: {}", get_error_message())))
     }
