b0cd859c05fd7af42943652337ef38d9e11bb006
lamka02sk <mail@samuelillo.com>
2024-04-25 18:44:39 +0200

    Simplified HMAC PHP example

--- hmac.php
+++ hmac.php
@@ -1,3 +1,3 @@
 <?php
 
-echo hash_hmac('sha256', 'data/news/ivankaprinitre.sk/2023/photo-1499750310107-5fef28a66643_35c82f_sm.jpg?dpr=3&q=10&w=100', 'bP47vDOPMqIUTDEeO1TTw1HOg1Y4HUrZcDTZdELhxM11ApHgwZKakB61zbFXRG') . "\n";
+echo hash_hmac('sha256', 'data/news/category/2026/photo-1499750310107-5fef28a66643_35c82f.jpg?dpr=2&q=10&w=100', 'bP47vDOPMqIUTDEeO1TTw1HOg1Y4HUrZcDTZdELhxM11ApHgwZKakB61zbFXRG') . "\n";
